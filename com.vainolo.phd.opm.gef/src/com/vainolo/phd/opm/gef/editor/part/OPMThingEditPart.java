296d7e22679e413e1bfa29d7263c09e7654610bc
Vainolo <vainolo@gmail.com>
2011-07-07 14:43:37 +0300

    * Tutorial 7 part 1 - added move and resize capabilities

--- com.vainolo.phd.opm.gef/src/com/vainolo/phd/opm/gef/editor/part/OPMThingEditPart.java
+++ com.vainolo.phd.opm.gef/src/com/vainolo/phd/opm/gef/editor/part/OPMThingEditPart.java
@@ -1,5 +1,8 @@
 package com.vainolo.phd.opm.gef.editor.part;
 
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
 
 import com.vainolo.phd.opm.gef.editor.figure.OPMThingFigure;
@@ -7,6 +10,13 @@ import com.vainolo.phd.opm.model.OPMThing;
 
 public abstract class OPMThingEditPart extends AbstractGraphicalEditPart {
 
+	private OPMThingAdapter adapter;
+	
+	public OPMThingEditPart() {
+		super();
+		adapter = new OPMThingAdapter();
+	}
+	
 	@Override protected void refreshVisuals() {
 		OPMThingFigure figure = (OPMThingFigure)getFigure();
 		OPMThing model = (OPMThing)getModel();
@@ -15,4 +25,39 @@ public abstract class OPMThingEditPart extends AbstractGraphicalEditPart {
 		figure.getNameLabel().setText(model.getName());
 		parent.setLayoutConstraint(this, figure, model.getConstraints());
 	}
+	
+	@Override public void activate() {
+		if(!isActive()) {
+			((OPMThing)getModel()).eAdapters().add(adapter);
+		}
+		super.activate();
+	}
+
+	@Override public void deactivate() {
+		if(isActive()) {
+			((OPMThing)getModel()).eAdapters().remove(adapter);
+		}
+
+		super.deactivate();
+	}	
+	
+	public class OPMThingAdapter implements Adapter {
+
+		// Adapter interface
+		@Override public void notifyChanged(Notification notification) {
+			refreshVisuals();
+		}
+
+		@Override public Notifier getTarget() {
+			return (OPMThing)getModel();
+		}
+
+		@Override public void setTarget(Notifier newTarget) {
+			// Do nothing.
+		}
+
+		@Override public boolean isAdapterForType(Object type) {
+			return type.equals(OPMThing.class);
+		}
+	}	
 }
--- com.vainolo.phd.opm.gef/src/com/vainolo/phd/opm/gef/editor/policy/OPMObjectProcessDiagramXYLayoutPolicy.java
+++ com.vainolo.phd.opm.gef/src/com/vainolo/phd/opm/gef/editor/policy/OPMObjectProcessDiagramXYLayoutPolicy.java
@@ -1,18 +1,45 @@
 package com.vainolo.phd.opm.gef.editor.policy;
 
+import com.vainolo.phd.opm.model.OPMObject;
+import com.vainolo.phd.opm.model.OPMObjectProcessDiagram;
+import com.vainolo.phd.opm.model.OPMProcess;
+import com.vainolo.phd.opm.model.OPMThing;
+
+import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.gef.EditPart;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.editpolicies.XYLayoutEditPolicy;
 import org.eclipse.gef.requests.CreateRequest;
 
 import com.vainolo.phd.opm.gef.editor.command.OPMThingCreateCommand;
-import com.vainolo.phd.opm.model.OPMObjectProcessDiagram;
-import com.vainolo.phd.opm.model.OPMThing;
+import com.vainolo.phd.opm.gef.editor.command.OPMThingChangeConstraintCommand;
 
+/**
+ * This class describes the commands that can be used to change the layout inside the
+ * ObjectProcessDiagram, Create new entities inside the ObjectProcessDiagram
+ * 
+ * @author vainolo
+ *
+ */
 public class OPMObjectProcessDiagramXYLayoutPolicy extends XYLayoutEditPolicy {
 
+	/**
+	 * Command created the user requests to change the constraint (size, location) of an object that is
+	 * part of an OPD.
+	 */
+	@Override protected Command createChangeConstraintCommand(EditPart child, Object constraint) {
+		OPMThingChangeConstraintCommand command = new OPMThingChangeConstraintCommand();
+		command.setModel((OPMThing) child.getModel());
+		command.setNewConstraint((Rectangle) constraint);
+		return command;
+	}
+
+	/**
+	 * Command created to add new things to the OPD.
+	 */
 	@Override protected Command getCreateCommand(CreateRequest request) {
 		Command retVal = null;
-		if(request.getNewObject() instanceof OPMThing) {
+		if(request.getNewObjectType().equals(OPMObject.class) || request.getNewObjectType().equals(OPMProcess.class)) {
 			OPMThingCreateCommand command = new OPMThingCreateCommand();
 			command.setLocation(request.getLocation());
 			command.setParent((OPMObjectProcessDiagram)(getHost().getModel()));
