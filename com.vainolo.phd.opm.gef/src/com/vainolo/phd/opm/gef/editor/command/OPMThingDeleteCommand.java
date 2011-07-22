dab271799cf8f94fdf83c2c88c3ca320aa5ad103
Vainolo <vainolo@gmail.com>
2011-07-22 10:38:16 +0300

    Finished tutorial 10: Deleting Connections and Fixing of Thing Delete Command

--- com.vainolo.phd.opm.gef/src/com/vainolo/phd/opm/gef/editor/command/OPMThingDeleteCommand.java
+++ com.vainolo.phd.opm.gef/src/com/vainolo/phd/opm/gef/editor/command/OPMThingDeleteCommand.java
@@ -1,25 +1,82 @@
 package com.vainolo.phd.opm.gef.editor.command;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.eclipse.gef.commands.Command;
 
+import com.vainolo.phd.opm.model.OPMLink;
 import com.vainolo.phd.opm.model.OPMObjectProcessDiagram;
 import com.vainolo.phd.opm.model.OPMThing;
 
-public class OPMThingDeleteCommand extends Command {
-
+/**
+ * Command used to delete a thing.
+ * The functionality of this class fairly closed so it is declared final. 
+ * @author vainolo
+ *
+ */
+public final class OPMThingDeleteCommand extends Command {
+	
+    /** Thing to be deleted. */
 	private OPMThing thing;
+	/** OPD that owns the thing. */
 	private OPMObjectProcessDiagram opd;
+	/** Incoming and outgoing links. */
+	private List<OPMLink> links;
+	/** Sources for the links that start or end at this thing. */
+	private Map<OPMLink, OPMThing> linkSources;
+	/** Targets for the links that start or end at this thing. */ 
+	private Map<OPMLink, OPMThing> linkTargets;
 
 	@Override
 	public void execute() {
+		detachLinks();
 		thing.setOpd(null);
 	}
 
 	@Override
 	public void undo() {
+		reattachLinks();
 		thing.setOpd(opd);
 	}
 
+	/**
+	 * Detach all links from the thing and from the other
+	 * connecting thing, storing the connection information in local
+	 * data structures.
+	 */
+	private void detachLinks() {
+		links = new ArrayList<OPMLink>();
+		linkSources = new HashMap<OPMLink, OPMThing>();
+		linkTargets = new HashMap<OPMLink, OPMThing>();
+		links.addAll(thing.getIncomingLinks());
+		links.addAll(thing.getOutgoingLinks());
+		for (OPMLink link : links) {
+			linkSources.put(link, link.getSource());
+			linkTargets.put(link, link.getTarget());
+			link.setSource(null);
+			link.setTarget(null);
+			link.setOpd(null);
+		}
+	}
+
+	/**
+	 * Reattach all links to their source and target things.
+	 */
+	private void reattachLinks() {
+		for (OPMLink link : links) {
+			link.setSource(linkSources.get(link));
+			link.setTarget(linkTargets.get(link));
+			link.setOpd(opd);
+		}
+	}
+	
+	/**
+	 * Set the thing to delete from the diagram.
+	 * @param thing the Thing to delete from the diagram.
+	 */
 	public void setThing(OPMThing thing) {
 		this.thing = thing;
 		this.opd = thing.getOpd();
--- com.vainolo.phd.opm.gef/src/com/vainolo/phd/opm/gef/editor/part/OPMLinkEditPart.java
+++ com.vainolo.phd.opm.gef/src/com/vainolo/phd/opm/gef/editor/part/OPMLinkEditPart.java
@@ -6,19 +6,34 @@ import org.eclipse.gef.EditPolicy;
 import org.eclipse.gef.editparts.AbstractConnectionEditPart;
 import org.eclipse.gef.editpolicies.ConnectionEndpointEditPolicy;
 
+import com.vainolo.phd.opm.gef.editor.policy.OPMLinkConnectionEditPolicy;
+
+/**
+ * {@link EditPart} for the {@link OPMLink} model element.
+ * @author vainolo
+ */
 public class OPMLinkEditPart extends AbstractConnectionEditPart {
 
+	/**
+	 * Create and initialize a new {@link OPMLinkEditPart}.
+	 */
 	public OPMLinkEditPart() {
 		super();
 	}
 	
+	/**
+	 * Installs two edit policies:
+	 * <ol>
+	 *   <li>For the {@link EditPolicy#CONNECTION_ENDPOINTS_ROLE} a {@link ConnectionEndpoinEditPolicy}.</li>
+	 *   <li>For the {@link EditPolicy#CONNECTION_ROLE} a {@link OPMLinkConnectionEditPolicy}</li>
+	 */
 	@Override protected void createEditPolicies() {
 		installEditPolicy(EditPolicy.CONNECTION_ENDPOINTS_ROLE, new ConnectionEndpointEditPolicy());
+		installEditPolicy(EditPolicy.CONNECTION_ROLE, new OPMLinkConnectionEditPolicy());		
 	}
 
 	@Override protected IFigure createFigure() {
 		PolylineConnection conn = new PolylineConnection();
 		return conn; 
 	}
-
 }
