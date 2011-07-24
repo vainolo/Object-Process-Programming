afa855959cdefe1217fe65da7eb4df410c3aba11
Vainolo <vainolo@gmail.com>
2011-07-24 11:17:58 +0300

    Finished tutorial 11 - Creating Link Bendpoints

--- com.vainolo.phd.opm.gef/src/com/vainolo/phd/opm/gef/editor/part/OPMLinkEditPart.java
+++ com.vainolo.phd.opm.gef/src/com/vainolo/phd/opm/gef/editor/part/OPMLinkEditPart.java
@@ -1,12 +1,24 @@
 package com.vainolo.phd.opm.gef.editor.part;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.draw2d.AbsoluteBendpoint;
+import org.eclipse.draw2d.BendpointConnectionRouter;
+import org.eclipse.draw2d.Connection;
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.PolylineConnection;
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.gef.EditPolicy;
 import org.eclipse.gef.editparts.AbstractConnectionEditPart;
 import org.eclipse.gef.editpolicies.ConnectionEndpointEditPolicy;
 
+import com.vainolo.phd.opm.gef.editor.policy.OPMLinkBendpointEditPolicy;
 import com.vainolo.phd.opm.gef.editor.policy.OPMLinkConnectionEditPolicy;
+import com.vainolo.phd.opm.model.OPMLink;
 
 /**
  * {@link EditPart} for the {@link OPMLink} model element.
@@ -14,26 +26,76 @@ import com.vainolo.phd.opm.gef.editor.policy.OPMLinkConnectionEditPolicy;
  */
 public class OPMLinkEditPart extends AbstractConnectionEditPart {
 
+	private OPMLinkAdapter adapter;
+	
 	/**
 	 * Create and initialize a new {@link OPMLinkEditPart}.
 	 */
 	public OPMLinkEditPart() {
 		super();
+		adapter = new OPMLinkAdapter();
 	}
 	
 	/**
 	 * Installs two edit policies:
 	 * <ol>
 	 *   <li>For the {@link EditPolicy#CONNECTION_ENDPOINTS_ROLE} a {@link ConnectionEndpoinEditPolicy}.</li>
-	 *   <li>For the {@link EditPolicy#CONNECTION_ROLE} a {@link OPMLinkConnectionEditPolicy}</li>
+	 *   <li>For the {@link EditPolicy#CONNECTION_ROLE} a {@link OPMLinkConnectionEditPolicy}.</li>
+	 *   <li>For the {@link EditPolicy#CONNECTION_BENDPOINTS_ROLE} a {@link OPMLinkBendpointEditPolicy}.</li>
+	 * </ol>
 	 */
 	@Override protected void createEditPolicies() {
 		installEditPolicy(EditPolicy.CONNECTION_ENDPOINTS_ROLE, new ConnectionEndpointEditPolicy());
-		installEditPolicy(EditPolicy.CONNECTION_ROLE, new OPMLinkConnectionEditPolicy());		
+		installEditPolicy(EditPolicy.CONNECTION_ROLE, new OPMLinkConnectionEditPolicy());
+		installEditPolicy(EditPolicy.CONNECTION_BENDPOINTS_ROLE, new OPMLinkBendpointEditPolicy());
 	}
 
 	@Override protected IFigure createFigure() {
 		PolylineConnection conn = new PolylineConnection();
+		conn.setConnectionRouter(new BendpointConnectionRouter());
 		return conn; 
+	}
+	
+	@Override protected void refreshVisuals() {
+		Connection connection = getConnectionFigure();
+		List<Point> modelConstraint = ((OPMLink)getModel()).getBendpoints();
+		List<AbsoluteBendpoint> figureConstraint = new ArrayList<AbsoluteBendpoint>();
+		for (Point p : modelConstraint) {
+			figureConstraint.add(new AbsoluteBendpoint(p));
+		}
+		connection.setRoutingConstraint(figureConstraint);
+	}
+	
+	@Override public void activate() {
+		if(!isActive()) {
+			((OPMLink)getModel()).eAdapters().add(adapter);
+		}
+		super.activate();
+	}
+
+	@Override public void deactivate() {
+		if(isActive()) {
+			((OPMLink)getModel()).eAdapters().remove(adapter);
+		}
+		super.deactivate();
+	}	
+	
+	public class OPMLinkAdapter implements Adapter {
+
+		@Override public void notifyChanged(Notification notification) {
+			refreshVisuals();
+		}
+
+		@Override public Notifier getTarget() {			
+			return (OPMLink)getModel();
+		}
+
+		@Override public void setTarget(Notifier newTarget) {
+			// Do nothing.
+		}
+
+		@Override public boolean isAdapterForType(Object type) {
+			return type.equals(OPMLink.class);
+		}
 	}
 }
