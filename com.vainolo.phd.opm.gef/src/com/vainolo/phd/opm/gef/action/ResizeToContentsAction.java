e6f097cd9e6e5d70d55af1881361c86d7ee74592
Arieh Bibliowicz <vainolo@gmail.com>
2012-02-21 11:13:20 +0200

    Fixed copyright of ResizeToContentsAction

--- com.vainolo.phd.opm.gef/src/com/vainolo/phd/opm/gef/action/ResizeToContentsAction.java
+++ com.vainolo.phd.opm.gef/src/com/vainolo/phd/opm/gef/action/ResizeToContentsAction.java
@@ -1,5 +1,7 @@
 /*******************************************************************************
- * This is me!!!
+ * Copyright (c) 2012 Arieh 'Vainolo' Bibliowicz
+ * You can use this code for educational purposes. For any other uses
+ * please contact me: vainolo@gmail.com
  *******************************************************************************/
 package com.vainolo.phd.opm.gef.action;
 
@@ -14,45 +16,47 @@ import com.vainolo.phd.opm.gef.editor.part.OPMNodeEditPart;
 
 public class ResizeToContentsAction extends SelectionAction {
 
-    public static final String RESIZE_TO_CONTENTS = "ResizeToContents";
-    public static final String RESIZE_TO_CONTENTS_REQUEST = "ResizeToContents";
+	public static final String RESIZE_TO_CONTENTS = "ResizeToContents";
+	public static final String RESIZE_TO_CONTENTS_REQUEST = "ResizeToContents";
 
-    Request request;
+	Request request;
 
-    public ResizeToContentsAction(IWorkbenchPart part) {
-        super(part);
-        setId(RESIZE_TO_CONTENTS);
-        setText("Resize to Contents");
-        request = new Request(RESIZE_TO_CONTENTS_REQUEST);
-    }
+	public ResizeToContentsAction(IWorkbenchPart part) {
+		super(part);
+		setId(RESIZE_TO_CONTENTS);
+		setText("Resize to Contents");
+		request = new Request(RESIZE_TO_CONTENTS_REQUEST);
+	}
 
-    @Override
-    public void run() {
-        // selected objects must be nodes because the action is enabled.
-        List<OPMNodeEditPart> editParts = getSelectedObjects();
-        CompoundCommand compoundCommand = new CompoundCommand();
-        for(OPMNodeEditPart nodeEditPart : editParts) {
-            compoundCommand.add(nodeEditPart.getCommand(request));
-        }
-        execute(compoundCommand);
-    }
+	@Override
+	public void run() {
+		// selected objects must be nodes because the action is enabled.
+		List<OPMNodeEditPart> editParts = getSelectedObjects();
+		CompoundCommand compoundCommand = new CompoundCommand();
+		for (OPMNodeEditPart nodeEditPart : editParts) {
+			compoundCommand.add(nodeEditPart.getCommand(request));
+		}
+		execute(compoundCommand);
+	}
 
-    /**
-     * {@inheritDoc}
-     * <p>The action is enabled if all the selected entities on the
-     * editor are {@link OPMNodeEditPart} instances</p> 
-     */
-    @Override
-    protected boolean calculateEnabled() {
-        if(getSelectedObjects().isEmpty()) {
-            return false;
-        }
-        for(Object selectedObject : getSelectedObjects()) {
-            if(selectedObject instanceof OPMNodeEditPart) {
-                return true;
-            }
-        }
-        return false;
-    }
+	/**
+	 * {@inheritDoc}
+	 * <p>
+	 * The action is enabled if all the selected entities on the editor are
+	 * {@link OPMNodeEditPart} instances
+	 * </p>
+	 */
+	@Override
+	protected boolean calculateEnabled() {
+		if (getSelectedObjects().isEmpty()) {
+			return false;
+		}
+		for (Object selectedObject : getSelectedObjects()) {
+			if (selectedObject instanceof OPMNodeEditPart) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
