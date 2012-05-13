53c05f7838395c9f1b5cd6720ecd62a5b4d5c994
Arieh Bibliowicz <vainolo@gmail.com>
2012-05-13 22:19:00 +0300

    Initial implementation of an eclipse launcher for and OPM file

---
+++ com.vainolo.phd.opm.gef/src/com/vainolo/phd/opm/gef/editor/OPMLaunchConfigurationDelegate.java
@@ -0,0 +1,16 @@
+package com.vainolo.phd.opm.gef.editor;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+
+public class OPMLaunchConfigurationDelegate implements ILaunchConfigurationDelegate {
+
+	@Override
+	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor)
+			throws CoreException {
+		System.out.println("Hello");
+	}
+}
