4eb2b48d2bf78193d2973138a598866fd3a354ea
Arieh Bibliowicz <vainolo@gmail.com>
2012-07-05 10:43:32 +0300

    OPEN - task 42: Intrepreter for OPM defined processes https://github.com/vainolo/Object-Process-Programming/issues/issue/42
    Added missing switch break.

--- com.vainolo.phd.opm.interpreter/src/com/vainolo/phd/opm/interpreter/OPMProcessInstanceFactoryImpl.java
+++ com.vainolo.phd.opm.interpreter/src/com/vainolo/phd/opm/interpreter/OPMProcessInstanceFactoryImpl.java
@@ -11,42 +11,43 @@ import com.vainolo.phd.opm.interpreter.builtin.OPMOutputProcessInstance;
 import com.vainolo.phd.opm.model.OPMProcessKind;
 
 public enum OPMProcessInstanceFactoryImpl implements OPMProcessInstanceFactory {
-	INSTANCE;
+  INSTANCE;
 
-	@Override
-	public OPMProcessInstance createProcessInstance(final String processName, final OPMProcessKind kind) {
-		// Create process instance.
-		OPMProcessInstance processInstance;
-		switch(kind) {
-		case BUILT_IN:
-			processInstance = createBuildInProcess(processName);
-			break;
-		case COMPOUND:
-			processInstance = new OPMCompoundProcessInstance(processName);
-		case CONCEPTUAL:
-			throw new UnsupportedOperationException("Conceptual processes are not supported yet.");
-		case JAVA:
-			throw new UnsupportedOperationException("Java processes are not supported yet.");
-		default:
-			throw new IllegalStateException("Received unexpected OPMProcessKind " + kind.getName());
-		}
+  @Override
+  public OPMProcessInstance createProcessInstance(final String processName, final OPMProcessKind kind) {
+    // Create process instance.
+    OPMProcessInstance processInstance;
+    switch(kind) {
+      case BUILT_IN:
+        processInstance = createBuildInProcess(processName);
+        break;
+      case COMPOUND:
+        processInstance = new OPMCompoundProcessInstance(processName);
+        break;
+      case CONCEPTUAL:
+        throw new UnsupportedOperationException("Conceptual processes are not supported yet.");
+      case JAVA:
+        throw new UnsupportedOperationException("Java processes are not supported yet.");
+      default:
+        throw new IllegalStateException("Received unexpected OPMProcessKind " + kind.getName());
+    }
 
-		return processInstance;
-	}
+    return processInstance;
+  }
 
-	private OPMProcessInstance createBuildInProcess(final String processName) {
-		OPMProcessInstance processInstance;
+  private OPMProcessInstance createBuildInProcess(final String processName) {
+    OPMProcessInstance processInstance;
 
-		if(processName.equals("Input"))
-			processInstance = new OPMInputProcessInstance();
-		else if(processName.equals("Output"))
-			processInstance = new OPMOutputProcessInstance();
-		else if(processName.equals("Add"))
-			processInstance = new OPMAddProcessInstance();
-		else
-			throw new IllegalStateException("Tried to create unexistent build-in process " + processName);
+    if(processName.equals("Input"))
+      processInstance = new OPMInputProcessInstance();
+    else if(processName.equals("Output"))
+      processInstance = new OPMOutputProcessInstance();
+    else if(processName.equals("Add"))
+      processInstance = new OPMAddProcessInstance();
+    else
+      throw new IllegalStateException("Tried to create unexistent build-in process " + processName);
 
-		return processInstance;
+    return processInstance;
 
-	}
+  }
 }
