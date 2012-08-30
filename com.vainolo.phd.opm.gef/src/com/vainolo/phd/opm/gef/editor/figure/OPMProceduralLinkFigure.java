7713dbe47e83abc5c5f0294b97793ab780984923
Arieh Bibliowicz <vainolo@gmail.com>
2012-08-30 10:15:14 +0300

    OPEN - task 69: Add states to execution of OPD https://github.com/vainolo/Object-Process-Programming/issues/issue/69
    * changed the look of the procedural links. Conditional links are
    decorated with a "c" besides the target, event links with an "e". This
    is until a better decoration is selected.

--- com.vainolo.phd.opm.gef/src/com/vainolo/phd/opm/gef/editor/figure/OPMProceduralLinkFigure.java
+++ com.vainolo.phd.opm.gef/src/com/vainolo/phd/opm/gef/editor/figure/OPMProceduralLinkFigure.java
@@ -8,7 +8,6 @@ package com.vainolo.phd.opm.gef.editor.figure;
 import org.eclipse.draw2d.Graphics;
 import org.eclipse.draw2d.PolylineConnection;
 import org.eclipse.draw2d.PolylineDecoration;
-import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.PointList;
 import org.eclipse.draw2d.geometry.Rectangle;
@@ -21,15 +20,15 @@ public class OPMProceduralLinkFigure extends PolylineConnection {
   private static final PolylineDecoration instrument = new PolylineDecoration();
 
   private static final int MIN_DECORATION_INTERVAL = 30;
-  private static final int SCALE = 4;
+  // private static final int SCALE = 4;
 
   private OPMProceduralLinkKind kind;
   private PolylineDecoration decoration;
 
   static {
-    arrow.setScale(SCALE, SCALE);
+    // arrow.setScale(SCALE, SCALE);
 
-    invertedArrow.setScale(SCALE, SCALE);
+    // invertedArrow.setScale(SCALE, SCALE);
     invertedArrow.setRotation(Math.PI);
 
     PointList instrumentPoints = new PointList();
@@ -66,52 +65,111 @@ public class OPMProceduralLinkFigure extends PolylineConnection {
   @Override
   protected void outlineShape(Graphics g) {
     g.drawPolyline(getPoints());
-    g.pushState();
-    drawDecorations(g);
-    g.popState();
-  }
+    PointList points = getPoints();
 
-  private void drawDecorations(Graphics g) {
-    Point p1, p2;
-    for(int i = 0; i < getPoints().size() - 1; i++) {
-      p1 = getPoints().getPoint(i);
-      p2 = getPoints().getPoint(i + 1);
+    Point source = points.getFirstPoint();
+    Point pointAfterSource = points.getPoint(1);
+    Point target = points.getLastPoint();
+    Point pointBeforeTarget = points.getPoint(points.size() - 2);
 
-      switch(kind) {
-        case CONSUMPTION:
-        case CONSUMPTION_CONDITION:
-        case CONSUMPTION_EVENT:
-        case RESULT:
-        case INSTRUMENT:
-        case INSTRUMENT_CONDITION:
-        case INSTRUMENT_EVENT:
-          drawDecorationsBetweenPoints(p1, p2, g);
-      }
+    switch(kind) {
+      case CONSUMPTION:
+      case CONSUMPTION_CONDITION:
+      case CONSUMPTION_EVENT:
+      case RESULT:
+      case EFFECT:
+      case EFFECT_CONDITION:
+      case EFFECT_EVENT:
+        arrow.setLocation(target);
+        arrow.setReferencePoint(pointBeforeTarget);
+        g.drawPolyline(arrow.getPoints());
+        break;
+      case INSTRUMENT:
+      case INSTRUMENT_CONDITION:
+      case INSTRUMENT_EVENT:
+        g.drawOval(target.x() - 5, target.y() - 5, 10, 10);
+        g.fillOval(target.x() - 4, target.y() - 4, 8, 8);
+        break;
     }
 
+    switch(kind) {
+      case EFFECT:
+      case EFFECT_CONDITION:
+      case EFFECT_EVENT:
+        arrow.setLocation(source);
+        arrow.setReferencePoint(pointAfterSource);
+        g.drawPolyline(arrow.getPoints());
+        break;
+    }
+
+    switch(kind) {
+      case INSTRUMENT_CONDITION:
+      case CONSUMPTION_CONDITION:
+      case EFFECT_CONDITION:
+        if(pointBeforeTarget.x() < target.x())
+          g.drawText("c", target.x() - 20, target.y() - 20);
+        else
+          g.drawText("c", target.x() + 20, target.y() - 20);
+        break;
+    }
+
+    switch(kind) {
+      case INSTRUMENT_EVENT:
+      case CONSUMPTION_EVENT:
+      case EFFECT_EVENT:
+        if(pointBeforeTarget.x() < target.x())
+          g.drawText("e", target.x() - 20, target.y() - 20);
+        else
+          g.drawText("e", target.x() + 20, target.y() - 20);
+    }
+    // g.pushState();
+    // drawDecorations(g);
+    // g.popState();
   }
 
-  private void drawDecorationsBetweenPoints(Point p1, Point p2, Graphics g) {
-    if(p1.getDistance(p2) < MIN_DECORATION_INTERVAL)
-      return;
-
-    Dimension difference = p2.getDifference(p1);
-    Point center = p1.getCopy().getTranslated(difference.width() / 2, difference.height() / 2);
-    decoration.setLocation(center);
-    decoration.setReferencePoint(p1);
-    // for an inverted line the reference point has to be changed: decoration.setReferencePoint(p2);
-    // the decoration should also be an inverted arrow.
-    // g.setForegroundColor(ColorConstants.red);
-    g.drawPolyline(decoration.getPoints());
-
-    drawDecorationsBetweenPoints(p1, center, g);
-    drawDecorationsBetweenPoints(center, p2, g);
-  }
+  // private void drawDecorations(Graphics g) {
+  // Point p1, p2;
+  // for(int i = 0; i < getPoints().size() - 1; i++) {
+  // p1 = getPoints().getPoint(i);
+  // p2 = getPoints().getPoint(i + 1);
+  //
+  // // switch(kind) {
+  // // case CONSUMPTION:
+  // // case CONSUMPTION_CONDITION:
+  // // case CONSUMPTION_EVENT:
+  // // case RESULT:
+  // // case INSTRUMENT:
+  // // case INSTRUMENT_CONDITION:
+  // // case INSTRUMENT_EVENT:
+  // // drawDecorationsBetweenPoints(p1, p2, g);
+  // // }
+  // }
+  //
+  // }
+  //
+  // private void drawDecorationsBetweenPoints(Point p1, Point p2, Graphics g) {
+  // if(p1.getDistance(p2) < MIN_DECORATION_INTERVAL)
+  // return;
+  //
+  // Dimension difference = p2.getDifference(p1);
+  // Point center = p1.getCopy().getTranslated(difference.width() / 2, difference.height() / 2);
+  // decoration.setLocation(center);
+  // decoration.setReferencePoint(p1);
+  // // for an inverted line the reference point has to be changed: decoration.setReferencePoint(p2);
+  // // the decoration should also be an inverted arrow.
+  // // g.setForegroundColor(ColorConstants.red);
+  // g.drawPolyline(decoration.getPoints());
+  //
+  // drawDecorationsBetweenPoints(p1, center, g);
+  // drawDecorationsBetweenPoints(center, p2, g);
+  // }
 
   @Override
   public Rectangle getBounds() {
-    bounds = super.getBounds();
-    bounds.expand(SCALE, SCALE);
+    if(bounds == null) {
+      bounds = super.getBounds();
+      bounds.expand(30, 30);
+    }
     return bounds;
   }
 }
--- com.vainolo.phd.opm.gef/src/com/vainolo/phd/opm/gef/editor/part/OPMEditPartFactory.java
+++ com.vainolo.phd.opm.gef/src/com/vainolo/phd/opm/gef/editor/part/OPMEditPartFactory.java
@@ -19,37 +19,36 @@ import com.vainolo.phd.opm.model.OPMStructuralLinkAggregator;
 
 public class OPMEditPartFactory implements EditPartFactory {
 
-	@Override
-	public EditPart createEditPart(EditPart context, Object model) {
-		EditPart part = null;
+  @Override
+  public EditPart createEditPart(EditPart context, Object model) {
+    EditPart part = null;
 
-		if (model instanceof OPMObjectProcessDiagram) {
-			part = new OPMObjectProcessDiagramEditPart();
-		} else if (model instanceof OPMObject) {
-			part = new OPMObjectEditPart();
-		} else if (model instanceof OPMProcess) {
-			part = new OPMProcessEditPart();
-		} else if (model instanceof OPMProceduralLink) {
-			// It is important for OPMProceduralLink to be before OPMLink
-			// because
-			// they have an is-a relation and we would get the wrong EditPart.
-			part = new OPMProceduralLinkEditPart();
-		} else if (model instanceof OPMLink) {
-			part = new OPMLinkEditPart();
-		} else if (model instanceof OPMStructuralLinkAggregator) {
-			part = new OPMStructuralLinkAggregatorEditPart();
-		} else if (model instanceof OPMState) {
-			part = new OPMStateEditPart();
-		} else if (model instanceof Label) {
-			part = new LabelEditPart();
-		} else {
-			throw new IllegalArgumentException("Model class " + model.getClass() + " not supported yet.");
-		}
+    if(model instanceof OPMObjectProcessDiagram) {
+      part = new OPMObjectProcessDiagramEditPart();
+    } else if(model instanceof OPMObject) {
+      part = new OPMObjectEditPart();
+    } else if(model instanceof OPMProcess) {
+      part = new OPMProcessEditPart();
+    } else if(model instanceof OPMProceduralLink) {
+      // It is important for OPMProceduralLink to be before OPMLink because they have an is-a relation and we would get
+      // the wrong EditPart if the order is changed.
+      part = new OPMProceduralLinkEditPart();
+    } else if(model instanceof OPMLink) {
+      part = new OPMLinkEditPart();
+    } else if(model instanceof OPMStructuralLinkAggregator) {
+      part = new OPMStructuralLinkAggregatorEditPart();
+    } else if(model instanceof OPMState) {
+      part = new OPMStateEditPart();
+    } else if(model instanceof Label) {
+      part = new LabelEditPart();
+    } else {
+      throw new IllegalArgumentException("Model class " + model.getClass() + " not supported yet.");
+    }
 
-		if (part != null) {
-			part.setModel(model);
-		}
+    if(part != null) {
+      part.setModel(model);
+    }
 
-		return part;
-	}
+    return part;
+  }
 }
