1fd690d8054fbaeda6882e86d97b099ad803f814
Arieh Bibliowicz <vainolo@gmail.com>
2013-03-10 08:42:55 +0200

    CLOSED - task 41: Divide tools in the palette into different groups https://github.com/vainolo/Object-Process-Programming/issues/issue/41
    Created two tools groups, one with general tools (select and marquee)
    and the other with the OPP basic tools. Left the rest of the tools in
    the class for future use.

--- com.vainolo.phd.opm.gef/src/com/vainolo/phd/opm/gef/editor/OPMGraphicalEditorPalette.java
+++ com.vainolo.phd.opm.gef/src/com/vainolo/phd/opm/gef/editor/OPMGraphicalEditorPalette.java
@@ -8,9 +8,16 @@ package com.vainolo.phd.opm.gef.editor;
 import org.eclipse.gef.palette.CombinedTemplateCreationEntry;
 import org.eclipse.gef.palette.ConnectionCreationToolEntry;
 import org.eclipse.gef.palette.CreationToolEntry;
+import org.eclipse.gef.palette.MarqueeToolEntry;
+import org.eclipse.gef.palette.PaletteDrawer;
+import org.eclipse.gef.palette.PaletteEntry;
 import org.eclipse.gef.palette.PaletteGroup;
 import org.eclipse.gef.palette.PaletteRoot;
+import org.eclipse.gef.palette.PaletteStack;
+import org.eclipse.gef.palette.PanningSelectionToolEntry;
 import org.eclipse.gef.palette.SelectionToolEntry;
+import org.eclipse.gef.palette.ToolEntry;
+import org.eclipse.gef.tools.MarqueeSelectionTool;
 import org.eclipse.jface.resource.ImageDescriptor;
 
 import com.vainolo.phd.opm.gef.editor.factory.LabelFactory;
@@ -40,11 +47,97 @@ public class OPMGraphicalEditorPalette extends PaletteRoot {
   PaletteGroup group;
 
   public OPMGraphicalEditorPalette() {
-    addGroup();
-    addSelectionTool();
-    addNodeTools();
-    addOPMProceduralLinkTools();
-    addOPMStructuralLinkTools();
+    add(createGeneralPaletteTools());
+    add(createBasicOPMPaletteTools());
+
+    // addGroup();
+    // addSelectionTool();
+    // addNodeTools();
+    // addOPMProceduralLinkTools();
+    // addOPMStructuralLinkTools();
+  }
+
+  private PaletteDrawer createGeneralPaletteTools() {
+    PaletteDrawer drawer = new PaletteDrawer("General");
+    ToolEntry tool = new PanningSelectionToolEntry();
+    drawer.add(tool);
+    setDefaultEntry(tool);
+
+    PaletteStack marqueeStack = new PaletteStack("Marquee Tools", "", null);
+
+    // NODES CONTAINED (default)
+    marqueeStack.add(new MarqueeToolEntry());
+
+    // NODES TOUCHED
+    MarqueeToolEntry marquee = new MarqueeToolEntry();
+    marquee.setToolProperty(MarqueeSelectionTool.PROPERTY_MARQUEE_BEHAVIOR, new Integer(
+        MarqueeSelectionTool.BEHAVIOR_NODES_TOUCHED));
+    marqueeStack.add(marquee);
+
+    // NODES CONTAINED AND RELATED CONNECTIONS
+    marquee = new MarqueeToolEntry();
+    marquee.setToolProperty(MarqueeSelectionTool.PROPERTY_MARQUEE_BEHAVIOR, new Integer(
+        MarqueeSelectionTool.BEHAVIOR_NODES_CONTAINED_AND_RELATED_CONNECTIONS));
+    marqueeStack.add(marquee);
+
+    // NODES TOUCHED AND RELATED CONNECTIONS
+    marquee = new MarqueeToolEntry();
+    marquee.setToolProperty(MarqueeSelectionTool.PROPERTY_MARQUEE_BEHAVIOR, new Integer(
+        MarqueeSelectionTool.BEHAVIOR_NODES_TOUCHED_AND_RELATED_CONNECTIONS));
+    marqueeStack.add(marquee);
+
+    // CONNECTIONS CONTAINED
+    marquee = new MarqueeToolEntry();
+    marquee.setToolProperty(MarqueeSelectionTool.PROPERTY_MARQUEE_BEHAVIOR, new Integer(
+        MarqueeSelectionTool.BEHAVIOR_CONNECTIONS_CONTAINED));
+    marqueeStack.add(marquee);
+
+    // CONNECTIONS TOUCHED
+    marquee = new MarqueeToolEntry();
+    marquee.setToolProperty(MarqueeSelectionTool.PROPERTY_MARQUEE_BEHAVIOR, new Integer(
+        MarqueeSelectionTool.BEHAVIOR_CONNECTIONS_TOUCHED));
+    marqueeStack.add(marquee);
+
+    marqueeStack.setUserModificationPermission(PaletteEntry.PERMISSION_NO_MODIFICATION);
+    drawer.add(marqueeStack);
+
+    return drawer;
+  }
+
+  private PaletteEntry createBasicOPMPaletteTools() {
+    PaletteDrawer drawer = new PaletteDrawer("OPP Basic");
+    drawer.add(new CreationToolEntry("Label", "Create new Label", new LabelFactory(), ImageDescriptor.createFromFile(
+        this.getClass(), "icons/label.ico"), ImageDescriptor.createFromFile(this.getClass(), "icons/label.ico")));
+
+    ToolEntry entry =
+        new CombinedTemplateCreationEntry("OPMObject", "Create a new Object", new OPMObjectFactory(),
+            ImageDescriptor.createFromFile(this.getClass(), "icons/object.ico"), ImageDescriptor.createFromFile(
+                this.getClass(), "icons/object.ico"));
+    entry.setToolClass(CreationAndDirectEditTool.class);
+    drawer.add(entry);
+
+    entry =
+        new CreationToolEntry("OPMProcess", "Create a new Process", new OPMProcessFactory(),
+            ImageDescriptor.createFromFile(this.getClass(), "icons/process.ico"), ImageDescriptor.createFromFile(
+                this.getClass(), "icons/process.ico"));
+    entry.setToolClass(CreationAndDirectEditTool.class);
+    drawer.add(entry);
+
+    entry =
+        new CreationToolEntry("OPMState", "Create a new State", new OPMStateFactory(), ImageDescriptor.createFromFile(
+            this.getClass(), "icons/state.ico"), ImageDescriptor.createFromFile(this.getClass(), "icons/state.ico"));
+    entry.setToolClass(CreationAndDirectEditTool.class);
+    drawer.add(entry);
+
+    drawer.add(new ConnectionCreationToolEntry("Instrument", "Create a new Instrument link",
+        new OPMInstrumentLinkFactory(), ImageDescriptor.createFromFile(this.getClass(), "icons/instrument.ico"),
+        ImageDescriptor.createFromFile(this.getClass(), "icons/instrument.ico")));
+
+    drawer.add(new ConnectionCreationToolEntry("Result", "Create a new Result link", new OPMResultLinkFactory(),
+        ImageDescriptor.createFromFile(this.getClass(), "icons/result.ico"), ImageDescriptor.createFromFile(
+            this.getClass(), "icons/result.ico")));
+
+    return drawer;
   }
 
   private void addSelectionTool() {
@@ -56,34 +149,6 @@ public class OPMGraphicalEditorPalette extends PaletteRoot {
   private void addGroup() {
     group = new PaletteGroup("OPM Controls");
     add(group);
-  }
-
-  private void addNodeTools() {
-    CreationToolEntry entry =
-        new CreationToolEntry("Label", "Create new Label", new LabelFactory(), ImageDescriptor.createFromFile(
-            this.getClass(), "icons/label.ico"), ImageDescriptor.createFromFile(this.getClass(), "icons/label.ico"));
-    group.add(entry);
-
-    entry =
-        new CombinedTemplateCreationEntry("OPMObject", "Create a new Object", new OPMObjectFactory(),
-            ImageDescriptor.createFromFile(this.getClass(), "icons/object.ico"), ImageDescriptor.createFromFile(
-                this.getClass(), "icons/object.ico"));
-    entry.setToolClass(CreationAndDirectEditTool.class);
-    group.add(entry);
-
-    entry =
-        new CreationToolEntry("OPMProcess", "Create a new Process", new OPMProcessFactory(),
-            ImageDescriptor.createFromFile(this.getClass(), "icons/process.ico"), ImageDescriptor.createFromFile(
-                this.getClass(), "icons/process.ico"));
-    entry.setToolClass(CreationAndDirectEditTool.class);
-    group.add(entry);
-
-    entry =
-        new CreationToolEntry("OPMState", "Create a new State", new OPMStateFactory(), ImageDescriptor.createFromFile(
-            this.getClass(), "icons/state.ico"), ImageDescriptor.createFromFile(this.getClass(), "icons/state.ico"));
-    entry.setToolClass(CreationAndDirectEditTool.class);
-    group.add(entry);
-
   }
 
   /**
