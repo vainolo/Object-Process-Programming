489565f2c665cd57ec80b5ce2687ee259df795bb
Arieh Biblowicz <vainolo@gmail.com>
2014-09-01 18:34:52 +0300

    Fixed bug that could cause wrong IDs when more than one editor was open. Static variables and global state are evil :-)

--- com.vainolo.phd.opm.gef/src/com/vainolo/phd/opm/gef/editor/factory/OPMIdManager.java
+++ com.vainolo.phd.opm.gef/src/com/vainolo/phd/opm/gef/editor/factory/OPMIdManager.java
@@ -5,17 +5,21 @@
  *******************************************************************************/
 package com.vainolo.phd.opm.gef.editor.factory;
 
+import java.util.logging.Logger;
+
 /**
  * 
  * @author Arieh 'Vainolo' Bibliowicz
  * 
  */
 public class OPMIdManager {
-  private static long _id = 0;
+  Logger logger = Logger.getLogger(OPMIdManager.class.getName());
+  private long _id = 0;
 
   public synchronized long getNextId() {
     long idToReturn = _id;
     _id++;
+    logger.info("Next id is " + _id);
     return idToReturn;
   }
 
