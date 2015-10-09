bfec4103ac7179840a0d6a386795dc2e1135d16e
Arieh Bibliowicz <vainolo@gmail.com>
2015-10-09 10:41:17 +0300

    #156: Auto Resize things when text changes Auto-resize of states ready.
    Task-Url:
    http://github.com/vainolo/Object-Process-Programming/issues/issue/156

--- com.vainolo.phd.opp.editor/src/com/vainolo/phd/opp/editor/part/OPPStateEditPart.java
+++ com.vainolo.phd.opp.editor/src/com/vainolo/phd/opp/editor/part/OPPStateEditPart.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.gef.CompoundSnapToHelper;
 import org.eclipse.gef.EditPolicy;
@@ -20,9 +21,12 @@ import org.eclipse.gef.SnapToGeometry;
 import org.eclipse.gef.SnapToGrid;
 import org.eclipse.gef.SnapToHelper;
 import org.eclipse.gef.editpolicies.SnapFeedbackPolicy;
+import org.eclipse.swt.widgets.Display;
 
+import com.vainolo.phd.opp.model.OPPProcess;
 import com.vainolo.phd.opp.model.OPPState;
 import com.vainolo.phd.opp.editor.figure.OPPNamedElementFigure;
+import com.vainolo.phd.opp.editor.figure.OPPProcessFigure;
 import com.vainolo.phd.opp.editor.figure.OPPStateFigure;
 import com.vainolo.phd.opp.editor.part.delegates.OPPDirectEditDelegate;
 import com.vainolo.phd.opp.editor.policy.OPPNamedEntityDirectEditPolicy;
@@ -47,7 +51,7 @@ public class OPPStateEditPart extends OPPNodeEditPart {
 
   @Override
   public void performRequest(Request req) {
-    if(req.getType() == RequestConstants.REQ_OPEN) {
+    if (req.getType() == RequestConstants.REQ_OPEN) {
       OPPDirectEditDelegate.performDirectEditing(this, ((OPPNamedElementFigure) getFigure()).getNameFigure());
     }
   }
@@ -60,25 +64,37 @@ public class OPPStateEditPart extends OPPNodeEditPart {
 
     figure.getNameFigure().setText(model.getName());
     figure.setValueState(model.isValue());
-    parent.setLayoutConstraint(this, figure,
-        new Rectangle(model.getX(), model.getY(), model.getWidth(), model.getHeight()));
+    parent.setLayoutConstraint(this, figure, new Rectangle(model.getX(), model.getY(), model.getWidth(), model.getHeight()));
+
+    Display.getCurrent().asyncExec(new Runnable() {
+      @Override
+      public void run() {
+        OPPState model = (OPPState) getModel();
+        OPPStateFigure figure = (OPPStateFigure) getFigure();
+        Dimension prefSize = figure.getPreferredSize();
+        if (prefSize.width != model.getWidth() || prefSize.height != model.getHeight()) {
+          model.setWidth(figure.getPreferredSize().width);
+          model.setHeight(figure.getPreferredSize().height);
+        }
+      }
+    });
   }
 
   /**
-   * Currently the class only adapts to create a {@link SnapToHelper} when the
-   * editor is in snapping mode (either to grid or to shapes).
+   * Currently the class only adapts to create a {@link SnapToHelper} when the editor is in snapping mode (either to
+   * grid or to shapes).
    */
   @Override
   public Object getAdapter(@SuppressWarnings("rawtypes") Class key) {
-    if(key == SnapToHelper.class) {
+    if (key == SnapToHelper.class) {
       List<SnapToHelper> helpers = new ArrayList<SnapToHelper>();
-      if(Boolean.TRUE.equals(getViewer().getProperty(SnapToGeometry.PROPERTY_SNAP_ENABLED))) {
+      if (Boolean.TRUE.equals(getViewer().getProperty(SnapToGeometry.PROPERTY_SNAP_ENABLED))) {
         helpers.add(new SnapToGeometry(this));
       }
-      if(Boolean.TRUE.equals(getViewer().getProperty(SnapToGrid.PROPERTY_GRID_ENABLED))) {
+      if (Boolean.TRUE.equals(getViewer().getProperty(SnapToGrid.PROPERTY_GRID_ENABLED))) {
         helpers.add(new SnapToGrid(this));
       }
-      if(helpers.size() == 0) {
+      if (helpers.size() == 0) {
         return null;
       } else {
         return new CompoundSnapToHelper(helpers.toArray(new SnapToHelper[0]));
