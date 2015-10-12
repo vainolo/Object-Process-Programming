f7c04f56450e29479e9951878a8699cae5b057a2
Arieh Bibliowicz <vainolo@gmail.com>
2015-10-12 12:09:36 +0300

    #156: Auto Resize things when text changes
    Implemented perspective with Package Explorer, Console and OPL.
    
    Task-Url:
    http://github.com/vainolo/Object-Process-Programming/issues/issue/156

---
+++ com.vainolo.phd.opp.editor/src/com/vainolo/phd/opp/editor/PerspectiveFactory.java
@@ -0,0 +1,14 @@
+package com.vainolo.phd.opp.editor;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class PerspectiveFactory implements IPerspectiveFactory {
+
+  @Override
+  public void createInitialLayout(IPageLayout layout) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
