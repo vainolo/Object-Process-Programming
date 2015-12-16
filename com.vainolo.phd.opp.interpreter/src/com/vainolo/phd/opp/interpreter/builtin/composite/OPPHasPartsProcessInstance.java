6aabb2e35fd0cddc8511c00c9184b804a4affebb
Arieh Bibliowicz <vainolo@gmail.com>
2015-12-16 21:06:15 +0200

    Added "Has Part" process

---
+++ com.vainolo.phd.opp.interpreter/src/com/vainolo/phd/opp/interpreter/builtin/composite/OPPHasPartsProcessInstance.java
@@ -0,0 +1,37 @@
+package com.vainolo.phd.opp.interpreter.builtin.composite;
+
+import java.util.List;
+
+import com.google.common.collect.Lists;
+import com.vainolo.phd.opp.interpreter.OPPAbstractProcessInstance;
+import com.vainolo.phd.opp.interpreter.OPPObjectInstance;
+import com.vainolo.phd.opp.interpreter.OPPParameter;
+import com.vainolo.phd.opp.interpreter.OPPObjectInstance.InstanceKind;
+
+public class OPPHasPartsProcessInstance extends OPPAbstractProcessInstance {
+
+  @Override
+  protected void executing() throws Exception {
+    OPPObjectInstance object = getArgument("object");
+    if (object.getAllPartIndexes().size() > 0) {
+      setArgument("answer", OPPObjectInstance.createFromValue("yes"));
+    } else {
+      setArgument("answer", OPPObjectInstance.createFromValue("no"));
+    }
+  }
+
+  @Override
+  public List<OPPParameter> getIncomingParameters() {
+    return Lists.newArrayList(new OPPParameter("object"));
+  }
+
+  @Override
+  public List<OPPParameter> getOutgoingParameters() {
+    return Lists.newArrayList(new OPPParameter("answer"));
+  }
+
+  @Override
+  public String getName() {
+    return "Has Parts";
+  }
+}
